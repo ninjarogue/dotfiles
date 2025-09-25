ff9f331927b01e86c92896ab6972abe8acdbaecd
aric jiang <aricjiang89@gmail.com>
2025-09-25 18:03:36 +0800

    add nvim keymaps.lua

---
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Map kj to escape in insert mode
+vim.keymap.set("i", "kj", "<Esc>", { desc = "Exit insert mode" })
+vim.keymap.set("n", "<leader>cp", function()
+  local filepath = vim.fn.expand("%:.")  -- :. gives path relative to current working directory
+  local line_number = vim.fn.line(".")
+  local result = filepath .. ":" .. line_number
+  vim.fn.setreg("+", result)
+  vim.notify("Copied: " .. result)
+end, { desc = "Copy file path:line to clipboard" })
