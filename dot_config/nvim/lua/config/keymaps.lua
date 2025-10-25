9e209dc9578aac9dafceeeaf0015e38b4c47bdeb
aric jiang <aricjiang89@gmail.com>
2025-10-26 01:31:10 +0800

    add and update omarchy config files

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -5,9 +5,13 @@
 -- Map kj to escape in insert mode
 vim.keymap.set("i", "kj", "<Esc>", { desc = "Exit insert mode" })
 vim.keymap.set("n", "<leader>cp", function()
-  local filepath = vim.fn.expand("%:.")  -- :. gives path relative to current working directory
+  local filepath = vim.fn.expand("%:.") -- :. gives path relative to current working directory
   local line_number = vim.fn.line(".")
   local result = filepath .. ":" .. line_number
   vim.fn.setreg("+", result)
   vim.notify("Copied: " .. result)
 end, { desc = "Copy file path:line to clipboard" })
+
+-- Center screen after search navigation
+vim.keymap.set("n", "n", "nzz") -- Center after n
+vim.keymap.set("n", "N", "Nzz") -- Center after N
