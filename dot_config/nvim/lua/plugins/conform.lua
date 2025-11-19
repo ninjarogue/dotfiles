553df7b824c98ab63dc17b0efcef269cf77e7983
aric jiang <aricjiang89@gmail.com>
2025-11-19 10:33:56 +0800

    update nvim lua configs

---
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/conform.nvim",
+  optional = true,
+  opts = {
+    formatters_by_ft = {
+      json = { "biome" },
+      jsonc = { "biome" },
+    },
+  },
+}
---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      hidden = true, -- Show hidden files
+    },
+    scroll = {
+      enabled = false, -- Disable scrolling animations
+    },
+  },
+}
