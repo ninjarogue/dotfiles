c9176d07a9568504752a7f80a7dd14f1997c532e
aric jiang <aricjiang89@gmail.com>
2025-11-20 20:02:46 +0800

    update snacks.lua

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,9 +1,6 @@
 return {
   "folke/snacks.nvim",
   opts = {
-    picker = {
-      hidden = true, -- Show hidden files
-    },
     scroll = {
       enabled = false, -- Disable scrolling animations
     },
