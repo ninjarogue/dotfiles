2fa4befd0b66292f674b4196783ecf43f78b0403
aric jiang <aricjiang89@gmail.com>
2025-11-30 01:30:02 +0800

    add new keymap

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -4,6 +4,7 @@
 
 -- Map kj to escape in insert mode
 vim.keymap.set("i", "kj", "<Esc>", { desc = "Exit insert mode" })
+
 vim.keymap.set("n", "<leader>cp", function()
   local filepath = vim.fn.expand("%:.") -- :. gives path relative to current working directory
   local line_number = vim.fn.line(".")
@@ -11,6 +12,15 @@ vim.keymap.set("n", "<leader>cp", function()
   vim.fn.setreg("+", result)
   vim.notify("Copied: " .. result)
 end, { desc = "Copy file path:line to clipboard" })
+
+vim.keymap.set("v", "<leader>cP", function()
+  local filepath = vim.fn.expand("%:.")
+  local start_line = vim.fn.line("'<")
+  local end_line = vim.fn.line("'>")
+  local result = filepath .. ":" .. start_line .. "-" .. end_line
+  vim.fn.setreg("+", result)
+  vim.notify("Copied: " .. result)
+end, { desc = "Copy file path:line range to clipboard" })
 
 -- Center screen after search navigation
 vim.keymap.set("n", "n", "nzz") -- Center after n
